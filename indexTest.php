5719d77ab38d1a307981c349b51228033707bde4
devopslead <35591900+devopslead@users.noreply.github.com>
2018-05-17 12:36:11 +0700

    Update indexTest.php

--- indexTest.php
+++ indexTest.php
@@ -3,7 +3,8 @@ require 'vendor/autoload.php';
 use PHPUnit\Framework\TestCase;
 class indexTest extends TestCase {
     public function testIndex() {
-        echo "failed";
-        exit(1);
+        echo "success";
+        // echo "failed";
+        // exit(1);
     }
 }
